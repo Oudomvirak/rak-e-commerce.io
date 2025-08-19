2ddf239a50d68828026b710388d7deed6c1fdead
Oudomvirak <oudomvirakyato@gmail.com>
2025-08-19 09:59:33 +0700

    Add project files

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mail = Mail(app)
 def send_order_to_telegram(message):
 
     token = "8089780599:AAFWT9Ys4cNjxx6U167Bdt5z_1oKAQ_TNco"
-    chat_id = "800414261"
+    chat_id = "6607733250"
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         "chat_id": chat_id,
